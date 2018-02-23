376df890ac606c762a58c33bbd3b7f1bf0f05f10
TricityGaming <36425229+TricityGaming@users.noreply.github.com>
2018-02-23 18:39:24 +0100

    Create multiple_upload.py

---
+++ multiple_upload.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+from youtube_upload import main, lib
+
+fpath = sys.argv[1]
+
+title = os.path.basename(fpath)
+title = os.path.splitext(title)[0]
+
+args = [
+    '--client-secrets=client_secrets.json',
+    '--privacy=private',
+    '--title=%s' % title,
+    fpath,
+]
+
+# debug
+# print(args)
+
+sys.exit(lib.catch_exceptions(main.EXIT_CODES, main.main, args))
